e5ac4b4ecbe5da8589bad643417aa83d9476af69
Sarah <sarahsilva@live.com>
2016-10-27 13:53:25 -0500

    first commit

---
+++ app/routes.js
@@ -0,0 +1,85 @@
+// app/routes.js
+module.exports = function(app, passport){
+
+	////////////////////////////
+	//home page (with login links)
+	////////////////////////////
+
+	app.get('/', function(req,res){
+		res.render('index.ejs'); // load the index.ejs file
+
+	});
+
+	////////////////////////////
+	//login
+	////////////////////////////
+	app.get('/login',function(req,res){
+		//render the page and pass in any flash data if it exists
+		res.render('login.ejs',{message: req.flash('loginMessage') });
+	});
+
+	//processs the login form
+
+	//app.post('/login', do all our passport stuff here)
+
+	////////////////////////////
+	//signup
+	////////////////////////////
+
+	//show the signup form
+	app.get('/signup', function(req,res){
+		//render the page and pass in any flash data if it exists
+		res.render('signup.ejs', {message: req.flash('signupMessage')});
+	});
+
+	//process the signup form
+	//app.post('/signup', do all our passport stuff here)
+
+	////////////////////////////
+	//// profile section
+	////////////////////////////
+	app.get('/profile', isLoggedIn, function(req,res){
+		res.render('profile.ejs',{
+			user:req.user // get the user user out of the session and pass to template
+
+		});
+	});
+
+
+	app.get('/logout', function(req, res){
+		req.logout();
+		res.redirect('/');
+	});
+
+	//route middleware to make sure a user is logged in
+
+	function isLoggedIn(req,res,next){
+		//is user is authenticated in the session , carry on
+		if(req.isAuthenticated()){
+			return next();
+		}
+
+		//if they aren't redirect them to the home page
+		res.redirect('/');
+	}
+
+
+	// process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+
+
+
+
+}
